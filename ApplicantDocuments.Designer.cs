1c34f7444e83675333fb7e61d44003e6a132df7c
solaricx <105065066+solaricx@users.noreply.github.com>
2026-06-08 01:51:00 +0800

    Add files via upload

---
+++ ApplicantDocuments.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ApplicantDocumentsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboDocumentType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.dgvDocuments = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRemarks = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboDocumentType
+            // 
+            this.cboDocumentType.FormattingEnabled = true;
+            this.cboDocumentType.Items.AddRange(new object[] {
+            "Resume",
+            "Valid ID",
+            "Transcript",
+            "Certificate"});
+            this.cboDocumentType.Location = new System.Drawing.Point(141, 100);
+            this.cboDocumentType.Name = "cboDocumentType";
+            this.cboDocumentType.Size = new System.Drawing.Size(121, 21);
+            this.cboDocumentType.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Document type:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(298, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "APPLICANT DOCUMENTS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(921, 63);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(394, 90);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(85, 38);
+            this.btnUpload.TabIndex = 4;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRefresh.Location = new System.Drawing.Point(600, 90);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(85, 38);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(138, 170);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(64, 16);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Missing:";
+            // 
+            // dgvDocuments
+            // 
+            this.dgvDocuments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocuments.Location = new System.Drawing.Point(24, 245);
+            this.dgvDocuments.Name = "dgvDocuments";
+            this.dgvDocuments.Size = new System.Drawing.Size(737, 239);
+            this.dgvDocuments.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 532);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Remarks:";
+            // 
+            // txtRemarks
+            // 
+            this.txtRemarks.Location = new System.Drawing.Point(100, 532);
+            this.txtRemarks.Multiline = true;
+            this.txtRemarks.Name = "txtRemarks";
+            this.txtRemarks.Size = new System.Drawing.Size(661, 170);
+            this.txtRemarks.TabIndex = 10;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(497, 90);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 38);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(793, 759);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtRemarks);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgvDocuments);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboDocumentType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboDocumentType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataGridView dgvDocuments;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRemarks;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ ApplicantDocuments.cs
@@ -0,0 +1,270 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ApplicantDocumentsForm
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString =
+            "server=localhost;port=3306;database=hr_recruitment_db;uid=root;pwd=KalelSQL123;";
+
+        private int applicantID = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDocumentTypes();
+            LoadDocuments();
+
+            txtRemarks.ReadOnly = true;
+            txtRemarks.BackColor = SystemColors.Control;
+
+            dgvDocuments.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvDocuments.MultiSelect = false;
+        }
+
+        // =========================
+        // LOAD TYPES
+        // =========================
+        private void LoadDocumentTypes()
+        {
+            cboDocumentType.Items.Clear();
+            cboDocumentType.Items.Add("Resume");
+            cboDocumentType.Items.Add("Valid ID");
+            cboDocumentType.Items.Add("Transcript");
+            cboDocumentType.Items.Add("Certificate");
+        }
+
+        // =========================
+        // LOAD DOCUMENTS
+        // =========================
+        private void LoadDocuments()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT DocumentID, DocumentType, FileName, FilePath,
+                           Status, Remarks, DateSubmitted
+                    FROM ApplicantDocuments
+                    WHERE ApplicantID = @ApplicantID";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ApplicantID", applicantID);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvDocuments.DataSource = dt;
+            }
+
+            CheckMissingRequirements();
+        }
+
+        // =========================
+        // UPLOAD
+        // =========================
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboDocumentType.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Select document type.");
+                    return;
+                }
+
+                if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                    return;
+
+                string sourcePath = openFileDialog1.FileName;
+                string fileName = Path.GetFileName(sourcePath);
+
+                string uploadsFolder = Path.Combine(Application.StartupPath, "Uploads");
+
+                if (!Directory.Exists(uploadsFolder))
+                    Directory.CreateDirectory(uploadsFolder);
+
+                string destinationPath = Path.Combine(
+                    uploadsFolder,
+                    DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + fileName);
+
+                File.Copy(sourcePath, destinationPath, true);
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string check = @"
+                        SELECT COUNT(*) 
+                        FROM ApplicantDocuments
+                        WHERE ApplicantID=@ApplicantID
+                        AND DocumentType=@DocumentType";
+
+                    MySqlCommand checkCmd = new MySqlCommand(check, conn);
+                    checkCmd.Parameters.AddWithValue("@ApplicantID", applicantID);
+                    checkCmd.Parameters.AddWithValue("@DocumentType", cboDocumentType.Text);
+
+                    bool exists = Convert.ToInt32(checkCmd.ExecuteScalar()) > 0;
+
+                    if (exists)
+                    {
+                        string update = @"
+                            UPDATE ApplicantDocuments
+                            SET FileName=@FileName,
+                                FilePath=@FilePath,
+                                Status='Submitted',
+                                DateSubmitted=NOW()
+                            WHERE ApplicantID=@ApplicantID
+                            AND DocumentType=@DocumentType";
+
+                        MySqlCommand cmd = new MySqlCommand(update, conn);
+                        cmd.Parameters.AddWithValue("@ApplicantID", applicantID);
+                        cmd.Parameters.AddWithValue("@DocumentType", cboDocumentType.Text);
+                        cmd.Parameters.AddWithValue("@FileName", fileName);
+                        cmd.Parameters.AddWithValue("@FilePath", destinationPath);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        string insert = @"
+                            INSERT INTO ApplicantDocuments
+                            (ApplicantID, DocumentType, FileName, FilePath, Status)
+                            VALUES
+                            (@ApplicantID, @DocumentType, @FileName, @FilePath, 'Submitted')";
+
+                        MySqlCommand cmd = new MySqlCommand(insert, conn);
+                        cmd.Parameters.AddWithValue("@ApplicantID", applicantID);
+                        cmd.Parameters.AddWithValue("@DocumentType", cboDocumentType.Text);
+                        cmd.Parameters.AddWithValue("@FileName", fileName);
+                        cmd.Parameters.AddWithValue("@FilePath", destinationPath);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Upload successful!");
+                LoadDocuments();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Upload Error: " + ex.Message);
+            }
+        }
+
+        // =========================
+        // REFRESH
+        // =========================
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadDocuments();
+            CheckMissingRequirements();
+        }
+
+        // =========================
+        // DELETE (FIXED)
+        // =========================
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvDocuments.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Select a document first.");
+                    return;
+                }
+
+                DataGridViewRow row = dgvDocuments.SelectedRows[0];
+
+                int documentID = Convert.ToInt32(row.Cells["DocumentID"].Value);
+
+                DialogResult result = MessageBox.Show(
+                    "Are you sure you want to delete this document?",
+                    "Confirm Delete",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.No)
+                    return;
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = @"
+                        DELETE FROM ApplicantDocuments
+                        WHERE DocumentID=@DocumentID
+                        AND ApplicantID=@ApplicantID";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@DocumentID", documentID);
+                    cmd.Parameters.AddWithValue("@ApplicantID", applicantID);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Deleted successfully!");
+                LoadDocuments();
+                CheckMissingRequirements();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Delete Error: " + ex.Message);
+            }
+        }
+
+        // =========================
+        // SHOW REMARKS
+        // =========================
+        private void dgvDocuments_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txtRemarks.Text =
+                    dgvDocuments.Rows[e.RowIndex].Cells["Remarks"].Value?.ToString() ?? "";
+            }
+        }
+
+        // =========================
+        // MISSING CHECK
+        // =========================
+        private void CheckMissingRequirements()
+        {
+            List<string> required = new List<string>
+            {
+                "Resume",
+                "Valid ID",
+                "Transcript",
+                "Certificate"
+            };
+
+            foreach (DataGridViewRow row in dgvDocuments.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                string doc = row.Cells["DocumentType"].Value?.ToString();
+                if (doc != null)
+                    required.Remove(doc);
+            }
+
+            lblStatus.Text = required.Count == 0
+                ? "Status: Complete"
+                : "Missing: " + string.Join(", ", required);
+        }
+
+        // SAFE STUB (prevents designer crash)
+        private void label2_Click(object sender, EventArgs e) { }
+    }
+}
