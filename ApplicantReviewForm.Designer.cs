186a0f41577606c96545323689f76e75f6a371c7
solaricx <105065066+solaricx@users.noreply.github.com>
2026-06-08 05:00:05 +0800

    Add files via upload

---
+++ ApplicantReviewForm.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace HRApplicantReview
+{
+    partial class ApplicantReviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cboStatusFilter = new System.Windows.Forms.ComboBox();
+            this.dgvApplicants = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvDocuments = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnLockApplication = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplicants)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.Azure;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.MediumBlue;
+            this.splitContainer1.Panel1.Controls.Add(this.dgvApplicants);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.splitContainer1.Panel2.Controls.Add(this.btnLockApplication);
+            this.splitContainer1.Panel2.Controls.Add(this.btnRefresh);
+            this.splitContainer1.Panel2.Controls.Add(this.panel3);
+            this.splitContainer1.Panel2.Controls.Add(this.txtAddress);
+            this.splitContainer1.Panel2.Controls.Add(this.txtPhone);
+            this.splitContainer1.Panel2.Controls.Add(this.txtEmail);
+            this.splitContainer1.Panel2.Controls.Add(this.txtName);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 641);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.cboStatusFilter);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 97);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Location = new System.Drawing.Point(-4, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(534, 97);
+            this.panel2.TabIndex = 1;
+
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(164, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(182, 11);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // cboStatusFilter
+            // 
+            this.cboStatusFilter.FormattingEnabled = true;
+            this.cboStatusFilter.Items.AddRange(new object[] {
+            "All",
+            "",
+            "Submitted",
+            "",
+            "Under Review",
+            "",
+            "Approved",
+            "",
+            "Rejected",
+            "",
+            "Locked"});
+            this.cboStatusFilter.Location = new System.Drawing.Point(12, 39);
+            this.cboStatusFilter.Name = "cboStatusFilter";
+            this.cboStatusFilter.Size = new System.Drawing.Size(164, 21);
+            this.cboStatusFilter.TabIndex = 4;
+            // 
+            // dgvApplicants
+            // 
+            this.dgvApplicants.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvApplicants.Location = new System.Drawing.Point(0, 97);
+            this.dgvApplicants.Name = "dgvApplicants";
+            this.dgvApplicants.Size = new System.Drawing.Size(275, 544);
+            this.dgvApplicants.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(24, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(24, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(24, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(24, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Address:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(171, 147);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(268, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(171, 189);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(268, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(171, 229);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(268, 20);
+            this.txtPhone.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(171, 264);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(268, 41);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel3.Controls.Add(this.dgvDocuments);
+            this.panel3.Location = new System.Drawing.Point(1, 341);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(529, 182);
+            this.panel3.TabIndex = 10;
+            // 
+            // dgvDocuments
+            // 
+            this.dgvDocuments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocuments.Location = new System.Drawing.Point(10, 17);
+            this.dgvDocuments.Name = "dgvDocuments";
+            this.dgvDocuments.Size = new System.Drawing.Size(507, 150);
+            this.dgvDocuments.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(76, 563);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(104, 32);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnLockApplication
+            // 
+            this.btnLockApplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLockApplication.Location = new System.Drawing.Point(270, 563);
+            this.btnLockApplication.Name = "btnLockApplication";
+            this.btnLockApplication.Size = new System.Drawing.Size(202, 32);
+            this.btnLockApplication.TabIndex = 12;
+            this.btnLockApplication.Text = "LockApplication";
+            this.btnLockApplication.UseVisualStyleBackColor = true;
+
+            // 
+            // ApplicantReviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "ApplicantReviewForm";
+            this.Text = "ApplicantReviewForm";
+            this.Load += new System.EventHandler(this.ApplicantReviewForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplicants)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocuments)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvApplicants;
+        private System.Windows.Forms.ComboBox cboStatusFilter;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLockApplication;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvDocuments;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
---
+++ ApplicantReviewForm.cs
@@ -0,0 +1,200 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace HRApplicantReview
+{
+    public partial class ApplicantReviewForm : Form
+    {
+        private string connectionString =
+            "server=localhost;port=3306;database=hr_recruitment_db;uid=root;pwd=KalelSQL123;";
+
+        private int selectedApplicantID = 0;
+
+        public ApplicantReviewForm()
+        {
+            InitializeComponent();
+            dgvApplicants.CellClick += dgvApplicants_CellClick;
+        }
+
+        private void ApplicantReviewForm_Load(object sender, EventArgs e)
+        {
+            LoadStatusFilter();
+            LoadApplicants();
+        }
+
+        // =========================
+        // LOAD APPLICANTS
+        // =========================
+        private void LoadApplicants(string search = "", string status = "All")
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT 
+                        ApplicantID,
+                        FirstName,
+                        LastName,
+                        Email
+                    FROM Applicants
+                    WHERE (@search = ''
+                        OR FirstName LIKE @searchLike
+                        OR LastName LIKE @searchLike
+                        OR Email LIKE @searchLike)";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@search", search);
+                cmd.Parameters.AddWithValue("@searchLike", "%" + search + "%");
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvApplicants.DataSource = dt;
+            }
+        }
+
+        // =========================
+        // SELECT APPLICANT
+        // =========================
+        private void dgvApplicants_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            var value = dgvApplicants.Rows[e.RowIndex].Cells["ApplicantID"].Value;
+
+            if (value == null || value == DBNull.Value)
+                return;
+
+            selectedApplicantID = Convert.ToInt32(value);
+
+            LoadProfile();
+            LoadDocuments();
+        }
+
+        // =========================
+        // LOAD PROFILE
+        // =========================
+        private void LoadProfile()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT FirstName, LastName, Email, ContactNumber, Address
+                    FROM Applicants
+                    WHERE ApplicantID = @ApplicantID";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ApplicantID", selectedApplicantID);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    txtName.Text = reader["FirstName"] + " " + reader["LastName"];
+                    txtEmail.Text = reader["Email"].ToString();
+                    txtPhone.Text = reader["ContactNumber"].ToString();
+                    txtAddress.Text = reader["Address"].ToString();
+                }
+            }
+        }
+
+        // =========================
+        // LOAD DOCUMENTS
+        // =========================
+        private void LoadDocuments()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT 
+                        d.DocumentID,
+                        r.RequirementName,
+                        d.FilePath,
+                        d.Status,
+                        d.Remarks,
+                        d.DateSubmitted
+                    FROM ApplicantDocuments d
+                    INNER JOIN RequirementTypes r 
+                        ON d.RequirementTypeID = r.RequirementTypeID
+                    WHERE d.ApplicantID = @ApplicantID";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ApplicantID", selectedApplicantID);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvDocuments.DataSource = dt;
+            }
+        }
+
+        // =========================
+        // LOCK APPLICATION
+        // =========================
+        private void btnLockApplication_Click(object sender, EventArgs e)
+        {
+            if (selectedApplicantID <= 0)
+            {
+                MessageBox.Show("Select an applicant first.");
+                return;
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                    UPDATE Applications
+                    SET CurrentStatus = 'Locked'
+                    WHERE ApplicantID = @ApplicantID";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ApplicantID", selectedApplicantID);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadApplicants();
+        }
+
+        // =========================
+        // FILTER + SEARCH
+        // =========================
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadApplicants(txtSearch.Text.Trim(), cboStatusFilter.Text);
+        }
+
+        private void cboStatusFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadApplicants(txtSearch.Text.Trim(), cboStatusFilter.Text);
+        }
+
+        private void LoadStatusFilter()
+        {
+            cboStatusFilter.Items.Clear();
+            cboStatusFilter.Items.Add("All");
+            cboStatusFilter.Items.Add("Draft");
+            cboStatusFilter.Items.Add("Submitted");
+            cboStatusFilter.Items.Add("Locked");
+            cboStatusFilter.Items.Add("Approved");
+            cboStatusFilter.Items.Add("Rejected");
+
+            cboStatusFilter.SelectedIndex = 0;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadApplicants();
+        }
+    }
+}
