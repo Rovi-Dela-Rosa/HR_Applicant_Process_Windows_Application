388d63f470386bb4ae731b62358835adaec3ab15
Nins023 <siwelonin13@gmail.com>
2026-06-10 00:05:38 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace HR_Applicant_Process_Windows_System_MAIN
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new HRAdminDashboard());
+            Application.Run(new Choice());
         }
     }
 }
